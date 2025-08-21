dcf9e38742a12f36a4a0a63357b2dd2532661ce7
Pranjal Choubay <your_github_email@example.com>
2025-08-21 14:48:37 +0530

    backend setup

--- product-finder/src/components/usePremiumScroll.js
+++ product-finder/src/components/usePremiumScroll.js
@@ -97,7 +97,7 @@ export function usePremiumScroll(products) {
     // we know the scrolling has stopped.
     const handleScroll = () => {
       clearTimeout(scrollStopTimer);
-      scrollStopTimer = setTimeout(onScrollStop, 20);
+      scrollStopTimer = setTimeout(onScrollStop, 5);
     };
     
     el.addEventListener('scroll', handleScroll);
